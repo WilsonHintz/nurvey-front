41d6292cc469f4b6774df4060be827b304bbe407
Gaston Alonso <gastondolly@gmail.com>
2017-11-04 23:45:58 -0300

    Corregido errores de operadores

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiEndPoint: "http://nurvey.sytes.net:8000"
+  //apiEndPoint: "http://nurvey.sytes.net:8000"
+  apiEndPoint: "http://192.168.1.11"
 };
