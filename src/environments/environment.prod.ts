4c49069b5f50e4f2c84b26e0f9933509593fc237
Unknown <guillermohintz@gmail.com>
2017-11-05 15:45:57 -0300

    environment back to normal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiEndPoint: "http://nurvey.sytes.net:8000"
-  apiEndPoint: "http://192.168.1.11"
+  apiEndPoint: "http://nurvey.sytes.net:8000"
+  //apiEndPoint: "http://192.168.1.11"
 };
